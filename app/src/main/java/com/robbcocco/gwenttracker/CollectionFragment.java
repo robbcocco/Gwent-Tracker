9441eaf10e7849911918efbcdba4462b8465d7ae
robbcocco <robertococco@outlook.com>
2018-03-07 16:24:23 +0100

    First commit

--- app/src/main/java/com/robbcocco/gwenttracker/CollectionFragment.java
+++ app/src/main/java/com/robbcocco/gwenttracker/CollectionFragment.java
@@ -10,13 +10,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.robbcocco.gwenttracker.database.entity.CardModel;
+import com.robbcocco.gwenttracker.database.entity.CategoryModel;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import static com.bumptech.glide.request.target.Target.SIZE_ORIGINAL;
 
 /**
  * Created by rober on 3/3/2018.
@@ -78,5 +84,83 @@ public class CollectionFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+
+    private class CollectionAdapter extends RecyclerView.Adapter<CollectionFragment.RecyclerViewHolder> {
+
+        private List<CardModel> cardModelList;
+
+        public CollectionAdapter(List<CardModel> cardModelList) {
+            this.cardModelList = cardModelList;
+        }
+
+        @Override
+        public CollectionFragment.RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new CollectionFragment.RecyclerViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.collection_card, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(final CollectionFragment.RecyclerViewHolder holder, final int position) {
+            final CardModel cardModel = cardModelList.get(position);
+
+            holder.cardArt.setImageResource(R.drawable.placeholder_card_low);
+            if (cardModel.getVariationModelList() != null &&
+                    !cardModel.getVariationModelList().isEmpty() &&
+                    cardModel.getVariationModelList().get(0).getArt_low() != null) {
+                GlideApp.with(holder.itemView)
+                        .load(cardModel.getVariationModelList().get(0).getArt_low().toString())
+                        .override(SIZE_ORIGINAL, SIZE_ORIGINAL)
+                        .placeholder(R.drawable.placeholder_card_low)
+                        .into(holder.cardArt);
+            }
+
+            holder.cardName.setText(cardModel.getName().get("en-US"));
+            holder.cardName.setSelected(true);
+            if (cardModel.getCategoryModelList() != null) {
+                String categories = null;
+                for (CategoryModel categoryModel : cardModel.getCategoryModelList()) {
+                    if (categories != null) {
+                        categories = categories + ", " + categoryModel.getName().get("en-US");
+                    } else {
+                        categories = categoryModel.getName().get("en-US");
+                    }
+                }
+                holder.cardCategories.setText(categories);
+                holder.cardCategories.setSelected(true);
+            }
+            holder.itemView.setTag(cardModel);
+        }
+
+        @Override
+        public int getItemCount() {
+            return cardModelList.size();
+        }
+
+        public void updateCardModelList(List<CardModel> cardModelList) {
+            this.cardModelList = cardModelList;
+            notifyDataSetChanged();
+        }
+    }
+
+
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private ImageView cardArt;
+        private TextView cardName;
+        private TextView cardCategories;
+
+        RecyclerViewHolder(View view) {
+            super(view);
+            itemView.setOnClickListener(this);
+            cardArt = (ImageView) view.findViewById(R.id.collection_card_art);
+            cardName = (TextView) view.findViewById(R.id.collection_card_name);
+            cardCategories = (TextView) view.findViewById(R.id.collection_card_categories);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(itemView.getContext(), String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+        }
     }
 }
