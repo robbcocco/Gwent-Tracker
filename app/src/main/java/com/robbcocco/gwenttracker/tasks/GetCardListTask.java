3c20f0d44e9ff09643640b08f42ea079dc89f43e
robbcocco <robertococco@outlook.com>
2018-03-15 18:33:31 +0100

    Task will now return a list of cards complete with details, instead of stubs

--- app/src/main/java/com/robbcocco/gwenttracker/tasks/GetCardListTask.java
+++ app/src/main/java/com/robbcocco/gwenttracker/tasks/GetCardListTask.java
@@ -6,9 +6,11 @@ import android.os.AsyncTask;
 import com.robbcocco.gwenttracker.database.CardDatabase;
 import com.robbcocco.gwenttracker.database.entity.CardModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by rober on 3/14/2018.
@@ -17,7 +19,6 @@ import java.util.concurrent.Executors;
 public class GetCardListTask extends AsyncTask<Context, Void, List<CardModel>> {
 
     private GetCardListCallback getCardListInterface;
-    private ExecutorService executor;
 
     public GetCardListTask(GetCardListCallback getCardListInterface) {
         this.getCardListInterface = getCardListInterface;
@@ -26,12 +27,27 @@ public class GetCardListTask extends AsyncTask<Context, Void, List<CardModel>> {
     @Override
     protected List<CardModel> doInBackground(Context... context) {
         CardDatabase mDb = CardDatabase.getDatabase(context[0]);
-        List<CardModel> cardModelList = mDb.cardDao().loadAllCards();
-        executor = Executors.newFixedThreadPool(10);
-        for (CardModel card : cardModelList) {
+        List<CardModel> cardModelStubList = mDb.cardDao().loadAllCards();
+
+        final List<CardModel> cardModelList = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        for (CardModel card : cardModelStubList) {
             if (isCancelled()) { break; }
-            new GetCardDetailTask(getCardListInterface, card.id)
+            GetCardDetailCallback getCardDetailCallback = new GetCardDetailCallback() {
+                @Override
+                public void updateAdapter(CardModel result) {
+                    cardModelList.add(result);
+                }
+            };
+            new GetCardDetailTask(getCardDetailCallback, card.id)
                     .executeOnExecutor(executor, context[0]);
+        }
+        executor.shutdown();
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         return cardModelList;
